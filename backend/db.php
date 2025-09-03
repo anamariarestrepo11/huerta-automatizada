84e369ed10c6239f31248e3a05fc134335ee6d04
Tomas Usuga <sgomezgomez851@gmail.com>
2025-09-03 16:45:11 -0500

    actual

---
+++ backend/db.php
@@ -0,0 +1,11 @@
+<?php
+$host = "127.0.0.1";
+$user = "root";
+$pass = "";
+$dbname = "prueba";
+
+$conn = new mysqli($host, $user, $pass, $dbname, 3306);
+if ($conn->connect_error) {
+    die("Error de conexion;" . $conn->connect_error);
+}
+?>
